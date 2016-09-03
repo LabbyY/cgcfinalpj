2a909d251281bde27758aeaa2e909843584d06aa
LabbyY <popantelop@live.com>
2016-09-03 16:03:48 +0800

    添加演员表单完成。但是还不能上传图片。

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,7 +1,20 @@
 class ArtistsController < ApplicationController
 def index
+  build_artist
   @artists = Artist.all
 end
+
+def create
+  build_artist
+  respond_to do |format|
+    if @artist.save
+      format.html { redirect_to artists_path }
+    else
+      format.html { render :new }
+    end
+  end
+end
+
 
 private
 def build_artist
@@ -9,7 +22,8 @@ def build_artist
   @artist.attributes = artist_params
 end
 def artist_params
-  params[:artist]
-  params[:artist] ? params[:artist] : {}
+  artist_params = params[:artist]
+  artist_params ? artist_params.permit(:artistname, :country, :genre, :introduction) : {}
 end
+
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,4 @@
 class Artist < ApplicationRecord
   scope :ordered, -> { ordered("artistname, created_at desc") }
+  mount_uploader :avatar, AvatarUploader
 end
